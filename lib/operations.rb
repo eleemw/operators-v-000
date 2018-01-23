410832a79ca9133afb255586f32e074e2b8881cf
Erica Murdock-Waters <ericahpa@gmail.com>
2018-01-23 07:58:18 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,8 +1,8 @@
 def unsafe?(speed)
-	if speed < 40 || speed > 60
-		true
+	if speed < 40 || speed > 60   # returns true if speed is less than 40 
+		true                        # returns true if speed is greater than 60
 	else
-		false
+		false                       # returns false if speed is between 40 and 60
 	end
 end
 
